03048474c2a09145f79145ffeec78a649a242c8e
David Lytikainen <77453517+david-lytikainen@users.noreply.github.com>
2026-07-21 20:03:54 -0400

    Update cron.sh

--- cron.sh
+++ cron.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
-cd /opt/SAS/sas-api
-git pull
-cd /opt/SAS/sas-ui
-git pull
 cd /opt/SAS
 git pull
 
-#echo -e "\n\n[$(date)] - Running $1" >> /opt/SAS/logs/cron.log
-#/home/agentbot/.local/bin/codex exec resume --last --model gpt-5.4 --dangerously-bypass-approvals-and-sandbox "$(cat "$1")" 2>&1 | tail -10 >> /opt/SAS/logs/cron.log
-#echo "[$(date)] - Done" >> /opt/SAS/logs/cron.log
+echo -e "\n\n$1  $(date)" >> /opt/SAS/logs/cron.log
+/home/agentbot/.local/bin/codex exec resume --last --model gpt-5.4 --dangerously-bypass-approvals-and-sandbox "$(cat "$1")" 2>&1 | tail -10 >> /opt/SAS/logs/cron.log
+echo "Done  $(date)" >> /opt/SAS/logs/cron.log
 
 cd /opt/SAS
-#git add . && git commit -m 'logs' && git push
+git add . && git commit -m 'logs' && git push
