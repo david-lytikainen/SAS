b33977d78486decd33ceb299a71e9a1c5529c1d3
agentbot <agentbot>
2026-09-11 14:28:04 +0000

    logs

---
+++ cron.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+cd /opt/SAS
+git pull
+
+/home/agentbot/.local/bin/codex exec resume --last --model gpt-5.6-terra -c model_reasoning_effort=medium --dangerously-bypass-approvals-and-sandbox "$(cat
+  /opt/SAS/cron)" 2>&1 | tail -10 >> /opt/SAS/cron.log
+echo -e "Done  $(date)\n\n" >> /opt/SAS/cron.log
+
+cd /opt/SAS
+git add . && git commit -m 'logs' && git push
